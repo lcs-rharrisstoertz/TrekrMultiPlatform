c8df25000e25ef12f95842e9b0b66492cd1c877f
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2022-01-19 18:57:20 -0500

    Populated the TipsList view, added Tip data type. Tips loaded from JSON.

--- Shared/Views/TipsList.swift
+++ Shared/Views/TipsList.swift
@@ -8,8 +8,34 @@
 import SwiftUI
 
 struct TipsList: View {
+    
+    //this view creates its own source of truth
+    let tips: [Tip]
+    
     var body: some View {
-        Text("Hello, TipsList view!")
+        List(tips, id: \.text) { tip in
+            
+            //if there are children on this particular tip...
+            if tip.children != nil {
+                Label(tip.text, systemImage: "quote.bubble")
+            } else {
+                Text(tip.text)
+            }
+        }
+    }
+    
+    //initializer pulls tips data from JSON
+    //populates the array
+    init() {
+        
+        //get a pointer to the file
+        let url = Bundle.main.url(forResource: "tips", withExtension: "json")!
+        
+        //load the contents of the json file
+        let data = try! Data(contentsOf: url)
+        
+        //convert the data from the JSON file into the array
+        tips = try! JSONDecoder().decode([Tip].self, from: data)
     }
 }
 
