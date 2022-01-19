d8e9f1855838577d13fa837f2a5513d415e52de7
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2022-01-19 10:04:30 -0500

    Added tips and map view and a tab bar to switch between them

---
+++ Shared/Views/TipsList.swift
@@ -0,0 +1,20 @@
+//
+//  TipsList.swift
+//  TrekrMultiPlatform
+//
+//  Created by Harris-Stoertz, Rowan on 2022-01-19.
+//
+
+import SwiftUI
+
+struct TipsList: View {
+    var body: some View {
+        Text("Hello, TipsList view!")
+    }
+}
+
+struct TipsList_Previews: PreviewProvider {
+    static var previews: some View {
+        TipsList()
+    }
+}
---
+++ Shared/Views/WorldMap.swift
@@ -0,0 +1,20 @@
+//
+//  WorldMap.swift
+//  TrekrMultiPlatform
+//
+//  Created by Harris-Stoertz, Rowan on 2022-01-19.
+//
+
+import SwiftUI
+
+struct WorldMap: View {
+    var body: some View {
+        Text("Hello, WorldMap view!")
+    }
+}
+
+struct WorldMap_Previews: PreviewProvider {
+    static var previews: some View {
+        WorldMap()
+    }
+}
